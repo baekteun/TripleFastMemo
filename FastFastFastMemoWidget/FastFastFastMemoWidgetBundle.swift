c4989a06a02a498d930e286fa77a03f362ed7ff9
baegteun <baegteun@gmail.com>
2022-12-29 23:35:12 +0900

    :sparkles: :: Widget

---
+++ FastFastFastMemoWidget/FastFastFastMemoWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  FastFastFastMemoWidgetBundle.swift
+//  FastFastFastMemoWidget
+//
+//  Created by 최형우 on 2022/12/29.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FastFastFastMemoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FastFastFastMemoWidget()
+        FastFastFastMemoWidgetLiveActivity()
+    }
+}
